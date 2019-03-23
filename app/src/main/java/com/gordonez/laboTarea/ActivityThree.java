c87692e0108957520b1dd4ab661580ba9a731cde
Gabriela Ordonez <00361516@uca.edu.sv>
2019-03-23 13:01:08 -0600

    Activity one and three

--- app/src/main/java/com/gordonez/laboTarea/ActivityThree.java
+++ app/src/main/java/com/gordonez/laboTarea/ActivityThree.java
@@ -1,13 +1,44 @@
 package com.gordonez.laboTarea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ActivityThree extends AppCompatActivity {
+
+    private EditText mEdit;
+    private Button mButton;
+}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_three);
-    }
+        setContentView(R.layout.activity_main);
+
+        mEdit = findViewById(R.id.et_nombre);
+        mEdit = findViewById(R.id.et_apellido);
+        mEdit = findViewById(R.id.et_email);
+
+        mButton = findViewById(R.id.b_submit);
+
+
+        mButton.setOnClickListener(v ->{
+            Intent mIntent = new Intent();
+
+            String textNombre = mEdit.getText().toString();
+            String textApellido = mButton.getText().toString();
+            String textEmail = mEdit.getText().toString();
+
+            if(mIntent != null){
+                mIntent.putExtra(Intent.EXTRA_TEXT, textNombre);
+                mIntent.putExtra(Intent.EXTRA_TEXT, textApellido);
+                mIntent.putExtra(Intent.EXTRA_TEXT, textEmail);
+
+                startActivity(mIntent);
+            }
+
+
+        });
 }
